f21c03d17e2db5fe3ff0e3ea474f2085116fe0c9
John Kouris <jdkouris@gmail.com>
2019-10-01 18:50:53 -0700

    Set up the layouts with storyboards Added the outlets/action for the detail view

--- Tasks/View Controllers/TaskDetailViewController.swift
+++ Tasks/View Controllers/TaskDetailViewController.swift
@@ -10,21 +10,18 @@ import UIKit
 
 class TaskDetailViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet weak var saveBarButtonItem: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveTapped(_ sender: Any) {
+        
     }
-    */
-
+    
 }
