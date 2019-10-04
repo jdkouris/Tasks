b544d52a8be441d50d01fecfbae4c439d8d43888
John Kouris <jdkouris@gmail.com>
2019-10-03 18:49:27 -0700

    Added priority to the initializer and created the TaskPriority enum

--- Tasks/Model/Task+Convenience.swift
+++ Tasks/Model/Task+Convenience.swift
@@ -9,12 +9,20 @@
 import Foundation
 import CoreData
 
+enum TaskPriority: String {
+    case low
+    case normal
+    case high
+    case critical
+}
+
 extension Task {
     
-    convenience init(name: String, notes: String? = nil, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    convenience init(name: String, notes: String? = nil, priority: TaskPriority = .normal, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.name = name
         self.notes = notes
+        self.priority = priority.rawValue
     }
     
 }
